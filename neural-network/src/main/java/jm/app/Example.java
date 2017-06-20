43b4f5a5eb21b85f8d8e0596b9d01a4a4796d81f
JiamingMai <jiamingmai@163.com>
2017-06-20 16:43:23 +0800

    Add some useful comments

--- neural-network/src/main/java/jm/app/Example.java
+++ neural-network/src/main/java/jm/app/Example.java
@@ -1,3 +1,17 @@
+/*
+ * This is an example of the neural network toolbox. The main method trys to train a neural network with three level to
+ * match the XOR operation. The network works successfully. The inputs and the corresponding correct outputs are as
+ * follows:
+ *   -----------------
+ *  | input | output |
+ *   -----------------
+ *  |  00   |   0    |
+ *  |  01   |   1    |
+ *  |  10   |   1    |
+ *  |  11   |   0    |
+ *  ------------------
+ */
+
 package jm.app;
 
 public class Example {
